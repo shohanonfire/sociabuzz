7bd12e3f9f1864861b54628562b817335ff85c90
S9_Encryption <tradershohan999@gmail.com>
2025-09-14 04:42:57 +0600

    Delete js directory

--- js/config.example.js
+++
@@ -1,3 +0,0 @@
-export const SERVER_BASE_URL = "http://localhost:3000";
-export const WHATSAPP_LINK = "https://wa.link/k5cz1f";
-export const DEFAULT_LANG = "bn";
--- js/track.js
+++
@@ -1,5 +0,0 @@
-import { api } from "./api.js";
-
-export async function track(event, meta={}, username){
-  try{ await api(`/api/track`, { method:"POST", body: JSON.stringify({ event, meta, username }) }); }catch{}
-}
