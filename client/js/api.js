23dedd33aa4e285cb56146d9949a07cc04cc3187
S9_Encryption <tradershohan999@gmail.com>
2025-09-14 09:01:49 +0600

    Update api.js

--- client/js/api.js
+++ client/js/api.js
@@ -1,4 +1,4 @@
-import { SERVER_BASE_URL } from "./config.js";
+import { SERVER_BASE_URL } from "/js/config.js";
 
 export async function api(path, opts={}){
   const res = await fetch(`${SERVER_BASE_URL}${path}`, {
