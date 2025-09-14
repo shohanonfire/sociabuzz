1eb4e1af99bbb9950bf46e12ec37e58221198d6d
S9_Encryption <tradershohan999@gmail.com>
2025-09-14 09:22:23 +0600

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -1,3 +1,3 @@
-export const SERVER_BASE_URL = "https://sociabuzz-production.up.railway.app";
+export const SERVER_BASE_URL = "/api";
 export const WHATSAPP_LINK = "https://wa.link/k5cz1f";
 export const DEFAULT_LANG = "bn";
